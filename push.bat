b090cc5903e70656df7f11d42aa318477d0b1573
Matthew Jallouk <jalloukmat@yahoo.com>
2016-08-13 13:04:56 -0400

    Tweaked mobile data site and improved search options.

---
+++ push.bat
@@ -0,0 +1,14 @@
+python push_aws.py -fi about.html -op A
+python push_aws.py -fi about-m.html -op A
+python push_aws.py -fi calendar.html -op A
+python push_aws.py -fi calendar-m.html -op A
+python push_aws.py -fi contact.html -op A
+python push_aws.py -fi contact-m.html -op A
+python push_aws.py -fi error.html -op A
+python push_aws.py -fi error-m.html -op A
+python push_aws.py -fi gallery.html -op A
+python push_aws.py -fi gallery-m.html -op A
+python push_aws.py -fi index.html -op A
+python push_aws.py -fi index-m.html -op A
+python push_aws.py -fi upcomingEvents.html -op A
+python push_aws.py -fi upcomingEvents-m.html -op A
